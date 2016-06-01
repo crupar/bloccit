6d70f88e4938851a48a5a5300dad67844359e0f4
Carolyn <crupar@gmail.com>
2016-06-01 15:12:09 -0700

    checkpoint 31: fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
 
-  get "about" => "welcom#about"
+  get "about" => "welcome#about"
   root "welcome#index"
   #root({to: 'welcome#index'})
 end
