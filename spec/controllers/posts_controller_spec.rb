9157b1c12e75ac1f66c6210e435ca518f567c8ef
Carolyn <crupar@gmail.com>
2016-06-02 11:01:48 -0700

    Checkpoint 32: completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,27 +16,35 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-#
-#  describe "GET #show" do
-#    it "returns http success" do
-#      get :show
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
-#
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+
+
 describe "GET new" do
   it "returns http success" do
     get :new
     expect(response).to have_http_status(:success)
   end
 
-# #2
   it "renders the #new view" do
     get :new
     expect(response).to render_template :new
   end
 
-# #3
   it "instantiates @post" do
     get :new
     expect(assigns(:post)).not_to be_nil
@@ -44,18 +52,15 @@ describe "GET new" do
 end
 
 describe "POST create" do
-# #4
   it "increases the number of Post by 1" do
     expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
   end
 
-# #5
   it "assigns the new post to @post" do
     post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
     expect(assigns(:post)).to eq Post.last
   end
 
-# #6
   it "redirects to the new post" do
     post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
     expect(response).to redirect_to Post.last
