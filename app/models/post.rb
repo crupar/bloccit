0fc42adefcb6c15437d835d80c387a054301db77
Carolyn <crupar@gmail.com>
2016-06-02 23:34:06 -0700

    Checkpoint 33: completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,4 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  after_save :filter_spam
-
-  private
-  def filter_spam
-    update(title: 'spam!') if self.id % 5 == 0
-  end
 end
