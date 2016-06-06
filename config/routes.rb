44e16f3cbab8a391ef1dc3e0a6aa9bbecedf00b2
Carolyn <crupar@gmail.com>
2016-06-06 16:14:27 -0700

    Assignment-37: completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-
+post 'users/confirm' => 'users#confirm'
 
   get "about" => "welcome#about"
   root "welcome#index"
