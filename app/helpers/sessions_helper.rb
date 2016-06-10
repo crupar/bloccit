f9f0b67c2e64d5f9dc518f3d2d0512f488e12131
Carolyn <crupar@gmail.com>
2016-06-09 23:36:01 -0700

    Assignment 38: completed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,9 @@ module SessionsHelper
      User.find_by(id: session[:user_id])
    end
 
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+
 end
