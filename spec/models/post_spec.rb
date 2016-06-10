f84c54990872618b186fd08dc1fdc5a854600f8d
Carolyn <crupar@gmail.com>
2016-06-10 10:45:51 -0700

    Checkpoint 41: comments not displaying yet

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Post, type: :model do
   let(:body) { RandomData.random_paragraph }
   let(:topic) { Topic.create!(name: name, description: description) }
 
+  it { is_expected.to have_many(:comments) }
+
+
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
@@ -27,6 +30,6 @@ RSpec.describe Post, type: :model do
   describe "attributes" do
     it "has a title, body, and user attribute" do
        expect(post).to have_attributes(title: title, body: body, user: user)
-     end   
+     end
   end
 end
