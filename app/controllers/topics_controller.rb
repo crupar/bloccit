190b0f80e6d125b54feb6f4dd307ccd09b27b57a
Carolyn <crupar@gmail.com>
2016-06-09 23:59:00 -0700

    Checkpoint-39: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,10 +12,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic = Topic.new(topic_params)
 
     if @topic.save
       flash[:notice] = "Topic was saved successfully."
@@ -33,9 +30,7 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
 
-    @topic.name = params[:topic][:name]
-    @topic.description = params[:topic][:description]
-    @topic.public = params[:topic][:public]
+    @topic.assign_attributes(topic_params)
 
     if @topic.save
        flash[:notice] = "Topic was updated successfully."
@@ -56,6 +51,10 @@ class TopicsController < ApplicationController
       flash.now[:alert] = "There was an error deleting the topic."
       render :show
     end
+  end
+private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
   end
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
 
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-  let(:comment) { Comment.create!(body: "Comment Body", post:post) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
   describe "attributes" do
     it "has a body attribute" do
