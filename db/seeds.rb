b53bb5696ab5cdec9f821c25f43c21bec06670fd
Carolyn <crupar@gmail.com>
2016-06-18 12:23:06 -0700

    Fixed seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,9 @@ topics.each { |t| rand(3..5).times { t.labels << Label.new(name: FFaker::Company
     body:   RandomData.random_paragraph
   )
 
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
 
 
@@ -70,3 +73,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
