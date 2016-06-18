9160a9e57cacf0b4359beda9026f9465a8d9404c
Carolyn <crupar@gmail.com>
2016-06-18 13:29:00 -0700

    Checkpoint-44: working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,9 +9,9 @@ class Comment < ActiveRecord::Base
 
   private
 
-    def send_favorite_emails
-      post.favorites.each do |favorite|
-        FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
-      end
+  def send_favorite_emails
+    post.favorites.each do |favorite|
+      FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
     end
+  end
 end
