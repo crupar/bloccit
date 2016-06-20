dd0ce9eb5db52540cf9c9e6975c96e4143d3a916
Carolyn <crupar@gmail.com>
2016-06-20 10:40:42 -0700

    Assignment-47: completed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,20 @@
-class VotesController < ApplicationController
+  class VotesController < ApplicationController
   before_action :require_sign_in
 
   def up_vote
     update_vote(1)
-    redirect_to :back
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def down_vote
     update_vote(-1)
-    redirect_to :back
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
