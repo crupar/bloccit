b8ba3af00c5414a2c63629ea62c2080c4a6aeac7
Carolyn <crupar@gmail.com>
2016-06-20 19:18:01 -0700

    Checkpoint-49: complete

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, only: [:index, :show]
+      resources :topics, except: [:edit, :new]
     end
   end
 
