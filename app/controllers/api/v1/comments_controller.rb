afab8f1e4ad99b600ca37e25988a7d33a7df4a75
Carolyn <crupar@gmail.com>
2016-06-20 21:25:17 -0700

    Assignment-48: specs don't pass

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::CommentsController < Api::V1::BaseController
+  before_filter :authenticate_user, expect: [:index, :show]
+  before_filter :authorize_user, expect: [:index, :show]
+
+  def index
+    comments = Comment.all
+    render json: comments.to_json, status: 200
+  end
+
+  def show
+    comment = Comment.find(params[:id])
+    render json: comment.to_json, status: 200
+  end
+
+
+end
